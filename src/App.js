f0ec505291675185c4efa10db1bd36f9c3404c53
daniraheim <daniraheim@gmail.com>
2025-03-28 22:03:45 -0500

    started nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// import { useState } from 'react';
+import Framework from './Framework';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  
+  //const [tasks, setTasks] = useState ([
+    //{id: 1, task: "car", description: "wash the car", status: false}
+  //])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Framework>
+          <Routes>
+            <Route path="/"/> 
+
+            <Route path="/add"/>
+
+            <Route path="/contact"/>
+     
+            </Routes>
+        </Framework>
+      </Router>
+    
   );
 }
 
---
+++ src/Framework.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+function Framework({children}) {
+    return(
+        <div className="container mt-4">
+            <h1 className="text-center mb-4">To-Do List</h1>
+            <nav>
+                <Link className="btn btn-primary me-2" to="/">List</Link>
+                <Link className="btn btn-success me-2" to="/add">Add New</Link>
+                <Link className="btn btn-info me-2" to="/contact">Contact Us</Link>
+            </nav>
+            {children}
+        </div>
+    )
+}
+export default Framework;
