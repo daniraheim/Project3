4c416da102be33a46574b3e485bf608f432f2b5b
daniraheim <daniraheim@gmail.com>
2025-04-02 17:46:08 -0500

    added NewContact component

---
+++ src/NewContact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ContactForm from "./ContactForm";
+
+function NewContact ({ onNew }) {
+    const navigate = useNavigate();
+
+    function handleContact (newContact) {
+        onNew(newContact);
+        navigate("/contact");
+    }
+
+    return <ContactForm 
+            initialData={{ firstName: "", lastName: "", email: "", comments: ""}} 
+            onSubmit={handleContact}/>   
+}
+
+export default NewContact;
